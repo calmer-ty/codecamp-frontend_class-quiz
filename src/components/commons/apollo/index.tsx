ee874d350a7d9aac3865fae892bfe4467be7ccfe
Calmer <ty.calmer96@gmail.com>
2024-03-28 00:01:37 +0900

    리프레시 토큰: 아폴로에서 작업중

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -1,9 +1,18 @@
-import { ApolloClient, ApolloLink, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
 
 import { createUploadLink } from "apollo-upload-client";
 import { useRecoilState, useRecoilValueLoadable } from "recoil";
-import { accessTokenState, restoreAccessTokenLoadable } from "../../../commons/stores";
+import {
+  accessTokenState,
+  restoreAccessTokenLoadable,
+} from "../../../commons/stores";
 import { useEffect } from "react";
+import { onError } from "@apollo/client/link/error";
 
 interface IApolloSetting {
   children: JSX.Element;
@@ -23,13 +32,26 @@ export default function ApolloSetting(props: IApolloSetting): JSX.Element {
     });
   }, []);
 
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    // 1. 에러 캐치
+    if (typeof graphQLErrors !== "undefined") {
+      for (const err of graphQLErrors) {
+        // 1-2. 해당 에러가 토큰만료 에러인지 체크
+        if (err.extensions.code === "UNAUTHENTICATED") {
+          // 2. 리프레시 토큰으로 accessToken 재발급 받기
+        }
+      }
+    }
+    // 3. 재발급 받은 accessToken으로 방금 실패한 쿼리 재요청하기
+  });
+
   const uploadLink = createUploadLink({
     uri: "http://backend-practice.codebootcamp.co.kr/graphql",
     headers: { Authorization: `Bearer ${accessToken}` },
   });
 
   const client = new ApolloClient({
-    link: ApolloLink.from([uploadLink]),
+    link: ApolloLink.from([errorLink, uploadLink]),
     cache: GLOBAL_STATE,
   });
   return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
