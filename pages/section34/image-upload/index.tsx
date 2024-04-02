b6f1100ef87a4d8a6455d3addb8b7c8d1964b748
Calmer_com <ty.calmer96@gmail.com>
2024-04-02 18:00:48 +0900

    파일 업로드 연습중

--- pages/section34/image-upload/index.tsx
+++ pages/section34/image-upload/index.tsx
@@ -3,15 +3,27 @@ import type { ChangeEvent } from "react";
 
 export default function ImageUpload(): JSX.Element {
   const [imageUrl, setImageUrl] = useState("");
+  const [file, setFile] = useState<File>();
 
   const onChangeFile = (event: ChangeEvent<HTMLInputElement>): void => {
     const file = event.target.files?.[0];
     if (file === undefined) return;
     console.log(file, "=> 파일까지 나옴");
 
-    // 1. 임시 Url 생성
+    // 1. 임시 Url 생성 내 브라우저만 가능 - 가짜
     const result = URL.createObjectURL(file);
     console.log(result);
+
+    // 2. 임시 Url 생성 다른 브라우저 접근 가능 - 진짜
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = (event) => {
+      console.log(event.target?.result);
+      if (typeof event.target?.result === "string") {
+        setImageUrl(event.target?.result);
+        setFile(file);
+      }
+    };
   };
   return (
     <>
@@ -25,7 +37,8 @@ export default function ImageUpload(): JSX.Element {
       <input type="text" />
       이미지:
       <input type="file" onChange={onChangeFile} />
-      <img src={imageUrl} alt="" />
+      <img src={imageUrl} />
+      {/* <button onClick={onClickSubmit}>등록하기</button> */}
     </>
   );
 }
