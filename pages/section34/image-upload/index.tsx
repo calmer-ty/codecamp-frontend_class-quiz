dbf2f02535c1dcee4a92dd1b3341b841c5e2f97d
Calmer <ty.calmer96@gmail.com>
2024-04-03 07:39:52 +0900

    lazyload 연습중

--- pages/section34/image-upload/index.tsx
+++ pages/section34/image-upload/index.tsx
@@ -1,7 +1,32 @@
+import { gql, useMutation } from "@apollo/client";
 import { useState } from "react";
 import type { ChangeEvent } from "react";
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from "../../../src/commons/types/generated/types";
+
+const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+    }
+  }
+`;
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
 
 export default function ImageUpload(): JSX.Element {
+  const [createBoard] = useMutation(CREATE_BOARD);
+  const [uploadFile] = useMutation<
+    Pick<IMutation, "uploadFile">,
+    IMutationUploadFileArgs
+  >(UPLOAD_FILE);
   const [imageUrl, setImageUrl] = useState("");
   const [file, setFile] = useState<File>();
 
@@ -25,6 +50,25 @@ export default function ImageUpload(): JSX.Element {
       }
     };
   };
+
+  const onClickSubmit = async (): Promise<void> => {
+    const resultFile = await uploadFile({ variables: { file } });
+    const url = resultFile.data?.uploadFile.url;
+
+    const result = await createBoard({
+      variables: {
+        createBoardInput: {
+          writer: "철수",
+          password: "1234",
+          title: "제목",
+          contents: "내용",
+          images: [url],
+        },
+      },
+    });
+    console.log(result);
+  };
+
   return (
     <>
       작성자:
@@ -38,7 +82,7 @@ export default function ImageUpload(): JSX.Element {
       이미지:
       <input type="file" onChange={onChangeFile} />
       <img src={imageUrl} />
-      {/* <button onClick={onClickSubmit}>등록하기</button> */}
+      <button onClick={onClickSubmit}>등록하기</button>
     </>
   );
 }
